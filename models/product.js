d6672a3965db3191c783d0a377ab5bd76e12e84a
solomon-prosperity <solomonprosperityera@gmail.com>
2021-07-11 00:36:03 +0800

    EcommerceApp

--- models/product.js
+++ models/product.js
@@ -1,7 +1,7 @@
 const products = [
     {
       id: 1,
-      name: 'Iphone',
+      name: 'couch and view',
       image:
         'https://dl.airtable.com/.attachments/6ac7f7b55d505057317534722e5a9f03/9183491e/product-3.jpg',
       price: 39.95,
@@ -9,7 +9,7 @@ const products = [
     },
     {
       id: 2,
-      name: 'Sony',
+      name: 'wall cabinet',
       image:
         'https://dl.airtable.com/.attachments/da5e17fd71f50578d525dd5f596e407e/d5e88ac8/product-2.jpg',
       price: 29.98,
@@ -17,7 +17,7 @@ const products = [
     },
     {
       id: 3,
-      name: 'LG Panorama',
+      name: 'relay armchair',
       image:
         'https://dl.airtable.com/.attachments/05ecddf7ac8d581ecc3f7922415e7907/a4242abc/product-1.jpeg',
       price: 10.99,
@@ -25,7 +25,7 @@ const products = [
     },
     {
       id: 4,
-      name: 'Nokia ',
+      name: ' four sitter couch',
       image:
         'https://dl.airtable.com/.attachments/3245c726ee77d73702ba8c3310639727/f000842b/product-5.jpg',
       price: 9.99,
